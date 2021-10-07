7a3351301c443ab2076455b5262d473f9f7cc1ef
Adam Binford <adamq43@gmail.com>
2021-10-06 19:27:28 -0700

    [SPARK-36918][SQL] Ignore types when comparing structs for unionByName
    Rather than using `DataType.sameType` for comparing structs when unioning by name, only compare the names and orders of the fields, since all we are doing is determining if we need to recreate the struct in a different order. After ResolveUnion is done, the normal union will handle if the types are incompatible or not.
    
    Additionally, adds a check to the recursive struct handling as well, so we don't have to recreate a nested struct if only one of its parents or siblings are different.
    
    Performance improvement, especially with nested nullable structs, which get wrapped in an If(IsNull()). Unioning three or more structs can explode the plan due to the multiple structs created extracting values from If(IsNull()) values when the projections are collapsed.
    
    No, just a performance improvement.
    
    New unit test for the helper method, and existing tests still pass.
    
    Closes #34166 from Kimahriman/union-by-name-ignore-types.
    
    Authored-by: Adam Binford <adamq43@gmail.com>
    Signed-off-by: Liang-Chi Hsieh <viirya@gmail.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/ResolveUnion.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/ResolveUnion.scala
@@ -51,7 +51,8 @@ object ResolveUnion extends Rule[LogicalPlan] {
       val currentField = colType.fields.find(f => resolver(f.name, expectedField.name))
 
       val newExpression = (currentField, expectedField.dataType) match {
-        case (Some(cf), expectedType: StructType) if cf.dataType.isInstanceOf[StructType] =>
+        case (Some(cf), expectedType: StructType) if cf.dataType.isInstanceOf[StructType]
+            && !DataType.equalsStructurallyByName(cf.dataType, expectedType, resolver) =>
           val extractedValue = ExtractValue(col, Literal(cf.name), resolver)
           addFields(extractedValue, expectedType)
         case (Some(cf), _) =>
@@ -101,7 +102,7 @@ object ResolveUnion extends Rule[LogicalPlan] {
         val foundDt = foundAttr.dataType
         (foundDt, lattr.dataType) match {
           case (source: StructType, target: StructType)
-              if allowMissingCol && !source.sameType(target) =>
+              if !DataType.equalsStructurallyByName(source, target, resolver) =>
             // We have two structs with different types, so make sure the two structs have their
             // fields in the same order by using `target`'s fields and then inluding any remaining
             // in `foundAttr`.
